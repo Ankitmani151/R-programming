079e5d479553c92f2407a01de235a29053597883
Ankit Mani <55287567+Ankitmani151@users.noreply.github.com>
2019-11-28 21:03:15 +0530

    Update Checking_Normal_Ditribution.R

--- Checking_Normal_Ditribution.R
+++ Checking_Normal_Ditribution.R
@@ -1,6 +1,9 @@
+#number of samples for which we are checking
+#higher the value of N output is more accurate answer=68.2
 N<-100000000
 counter<-0
 answer<-0
+#In R i is not index but the value of position itself
 for (i in rnorm(N)) {
   if(i>-1 & i<1){
     counter<-counter+1
