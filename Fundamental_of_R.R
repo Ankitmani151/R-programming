5ada2d03e5ae891ab80cc674d90f301aa757ec88
Ankit Mani <55287567+Ankitmani151@users.noreply.github.com>
2019-11-29 21:52:08 +0530

    Add files via upload

---
+++ Fundamental_of_R.R
@@ -0,0 +1,52 @@
+
+#we use fuction combine 'c' to define vectors
+MyFirstVector<- c(1,45,86,35,21,27)
+MyFirstVector
+
+#values are assumed as float by  defalut, To use the as integers use 'L'
+intege_check<- c(5L,12L,15L,19L)
+
+#creating charachter vector
+charachter_check<- c("a","ghh","67","9Ad")
+
+#all the elements of vector will be same data type
+
+#use of sequence 'seq' funtion
+seq(1,15) #ouput looks line  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+
+
+z<-seq(1,15,4)
+z    # output [1]  1  5  9 13
+
+
+#repetation 'rep' function is used to replicate
+rep(3,8) #output [1] 3 3 3 3 3 3 3 3
+
+
+#accesing a particular number from vector
+w<-c(41,5,9,48,56)  #created vector w
+w[2]
+
+#accessing some values from vetor
+w[2:5] #output [1]  5  9 48 56
+
+#accesing some diffrent values
+w[c(1,3,5)]  #output [1] 41  9 56
+
+#operation on two vectors (we can use '+','-','*','/','>')
+v1<-c(5,63,12,26)
+v2<-c(1,26,3,44)
+z1<-v1+v2
+z1   #output [1]  6 89 15 70
+
+#recycling of vector
+w1<-c(5,10,15,20)
+w2<-c(1,2,3,4,5,6,7,8,9)
+w1+w2 #output [1]  6 12 18 24 10 16 22 28 14
+
+
+
+
+
+
+
